0dc09a59894e208c3e1519e0b13d4476cca45531
AyushNimiwal <ayushnimiwal47@gmail.com>
2023-12-03 18:49:01 +0530

    color

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -37,7 +37,7 @@ function Layout() {
       <div className=' relative '>
         <div className=' absolute w-64 h-64 p-[-2.5rem] -top-28 -left-12 rounded-full bg-slate-300 z-[-1000] bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))]  from-indigo-900 to-sky-400 blur-2xl'></div>
         <div className=' absolute w-48 h-48 p-[-2.5rem] circle -top-10 left-2/4 rounded-full bg-slate-300 z-[-1000] bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))]  from-indigo-900 to-sky-400 blur-2xl'></div>
-        <div className=' absolute w-48 h-48 p-[-2.5rem] -top-36 right-0 rounded-full bg-slate-300 z-[-1000] bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))]  from-indigo-900 to-sky-400 blur-2xl'></div>
+        <div className=' absolute w-48 h-48 p-[-2.5rem] -top-36 right-0 rounded-full bg-slate-300 z-[-1000] bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))]  from-indigo-900 to-sky-400 blur-3xl'></div>
         <div className=' absolute w-72 h-72 m-[2.5rem] circle top-96 left-96 rounded-full bg-slate-300 z-[-1000] bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))]  from-indigo-900 to-sky-400 blur-2xl'></div>
         <div className=' absolute w-48 h-48 m-[2.5rem] top-56 -right-10 rounded-full bg-slate-300 z-[-1000] bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))]  from-indigo-900 to-sky-400 blur-2xl'></div>
         <div className='w-full px-6 flex justify-between'> 
